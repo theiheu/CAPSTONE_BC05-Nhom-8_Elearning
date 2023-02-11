3e3e206f2be38b9bdc456078e6683a022963971b
The Hieu <thehieu236@gmail.com>
2023-02-11 20:00:20 +0700

    add boostrap

--- src/utilities/Settings/config.js
+++ src/utilities/Settings/config.js
@@ -3,8 +3,9 @@
 export const DOMAIN = "https://elearningnew.cybersoft.edu.vn";
 export const TOKEN = "accessToken";
 export const GROUPID = "GP01";
-export const TokenCyberSoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNzEiLCJIZXRIYW5TdHJpbmciOiIyOS8xMi8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NzIyNzIwMDAwMDAiLCJuYmYiOjE2NDU5ODEyMDAsImV4cCI6MTY3MjQxOTYwMH0.SZe3CJl1OkNH-0zfzqOV0CSC8WZ6q2hw64UykpCytT0";
-export const TYPE_USER = 'TYPE_USER'; 
+export const TokenCyberSoft =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwNSIsIkhldEhhblN0cmluZyI6IjAxLzA2LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4NTU3NzYwMDAwMCIsIm5iZiI6MTY2MjMxMDgwMCwiZXhwIjoxNjg1NzI1MjAwfQ.Dy6ptxrq8hSICX31ALneG5jR5A2yC8FKcoHtGfXpSi0";
+export const TYPE_USER = "TYPE_USER";
 export const USER_SIGNIN = "USER_SIGNIN";
 
 export const CATE_BACKEND = "BackEnd";
@@ -15,4 +16,3 @@ export const CATE_FULLSTACK = "FullStack";
 export const CATE_TUDUY = "TuDuy";
 
 // export const USER_SIGNIN = "USER_SIGNIN";
-
